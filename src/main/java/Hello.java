182557d88dfc0970ffe30edfe175274aa7f246bd
Kim-SaeByeol <toquf345@gmail.com>
2023-03-15 09:33:44 +0900

    study

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 
         System.out.println("(" + "안녕하세요" + ")");
         System.out.println("Hello!! My First Java Coding!! ");
-
+        System.out.println("확인 됬습니다.");
     }
 }
