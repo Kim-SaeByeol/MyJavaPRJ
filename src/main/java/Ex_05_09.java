1fae8b9f355f87f7a7eefe39f7c6d8bc75f87477
Kim-SaeByeol <toquf345@gmail.com>
2023-03-29 10:28:08 +0900

    study

--- src/main/java/Ex_05_09.java
+++ src/main/java/Ex_05_09.java
@@ -11,7 +11,18 @@ public class Ex_05_09 {
         switch (a) {        //스위치의 결과 값은 a.
             case 1 :        // a 의 값이 1 일 경우 실행.
                 System.out.printf("1을 선택했다.\n");
-                break;      //switch 문을 벗어남.
+                //   break;      //switch 문을 벗어남.
+            case 2 :        // a 의 값이 2 일 경우 실행.
+                System.out.printf("2을 선택했다.\n");
+               // break;      //switch 문을 벗어남.
+            case 3 :        // a 의 값이 3 일 경우 실행.
+                System.out.printf("3을 선택했다.\n");
+             //   break;      //switch 문을 벗어남.
+            case 4 :        // a 의 값이 4 일 경우 실행.
+                System.out.printf("4을 선택했다.\n");
+               // break;      //switch 문을 벗어남.
         }
+        //break 를 안 걸면 멈추지 않고 쭉 실행.
+        // else if 는 선택이 5개 이하일때, switch 는 조건 선택이 5개 초과 일때 쓰자.
     }
 }
