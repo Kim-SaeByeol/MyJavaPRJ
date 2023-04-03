7f027589aef8c1d941de06b189cffcfbee36f3a5
Kim-SaeByeol <toquf345@gmail.com>
2023-04-03 19:14:41 +0900

    2023.04.03 19:10 자바 for문 연습

---
+++ src/main/java/For_Star_test3.java
@@ -0,0 +1,22 @@
+public class For_Star_test3 {
+    /*
+    별로 ▶ 모양 찍기.
+     */
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 5; i ++){
+            for (j = 0; j < 5; j++) {
+                if (j <= i)
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                if (i <= j)
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
