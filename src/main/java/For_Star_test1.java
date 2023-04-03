c415b8e6a1f3dac863cdbcb9c7fc1ef30df8eb66
Kim-SaeByeol <toquf345@gmail.com>
2023-04-03 19:14:16 +0900

    2023.04.03 19:10 자바 for문 연습

---
+++ src/main/java/For_Star_test1.java
@@ -0,0 +1,13 @@
+public class For_Star_test1 {
+    /*
+    별 직사각형 찍기
+     */
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
