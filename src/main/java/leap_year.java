5b8c6619c18bb09deabf6cffb88c3e0af9bbae04
Kim-SaeByeol <toquf345@gmail.com>
2023-04-05 11:43:20 +0900

    윤년

--- src/main/java/leap_year.java
+++ src/main/java/leap_year.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class year {
+public class leap_year {
         public static void main(String[] args) {
             int a ;
             Scanner sc = new Scanner(System.in);
