85d2826ffa2011e69890459d146a5013f9d179e7
Kim-SaeByeol <toquf345@gmail.com>
2023-04-05 11:41:12 +0900

    2023.04.05 11:40 자바 수업교재 예제

---
+++ src/main/java/Ex_06_17.java
@@ -0,0 +1,10 @@
+public class Ex_06_17 {
+    public static void main(String[] args) {
+        int i;
+        i = 0;
+        for ( ; ;) {        //for문 무한루프.
+            System.out.printf("%d \n", i);
+            i++;    //i 값을 계속 1씩 늘리는 상태로 for문을 반복시킴.
+        }
+    }
+}
