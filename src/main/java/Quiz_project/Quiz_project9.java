48236b7ae7303ddd0783cae818194a64c2c7ffc4
Kim-SaeByeol <toquf345@gmail.com>
2023-04-08 16:30:15 +0900

    23.04.06 교수님 과제 수정 https://band.us/band/88956828/post/107

--- src/main/java/Quiz_project/Quiz_project9.java
+++ src/main/java/Quiz_project/Quiz_project9.java
@@ -3,50 +3,56 @@ package Quiz_project;
 import java.util.Scanner;
 
 //입력받은 숫자가 5개(중복불가)일때, 그 숫자들을 크기가 작은순으로 정렬하게 코딩
+//만약 중복이라면 "중복입니다" 를 알린 후 다시 입력하도록 설정.
 public class Quiz_project9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //number = 배열의 수를 결정
+        int number;
+        System.out.println("몇 칸의 배열을 만들 것인가? : 1~100 사이 정수입력");
+        number = sc.nextInt();
 
-        //배열에 넣을 값으로 Number 변수 선언.
-        int Number;
-        //정수타입 배열 Array에 5칸 을 줌.
-        int Array[] = new int[5];
+        //입력 받은 수 만큼 배열 생성
+        int arr[] = new int[number];
 
-        //배열 Array에 인덱스 0 부터 4까지 순서대로 정수값을 입력하여 저장함.
-        for (int i = 0; i < Array.length; i++) {
-            Number = sc.nextInt();
-            Array[i] = Number;
-        }
-        //중복이라면 return 을 통해 반환값이 없는 void 메소드에서 빠져나감. => main 메소드를 빠져나가기에 그대로 종료.
-        for(int i = 0; i < Array.length; i++) {
-            for(int j = i+1; j < Array.length; j++){
-                if (Array[i] == Array[j]) {
-                    System.out.printf("중복 값이 있네요! 프로그램 종료!");
-                    return;
+        //배열에 넣을 값을 입력 받는 volume.
+        int volume;
+        System.out.println("배열 안에 값을 넣어보자.");
+        System.out.println("1~100 사이 정수. 입력은 하나씩만 받습니다. (입력 후 엔터를 눌러주세요!)");
+        for(int i = 0; i < arr.length; i++) {
+            volume = sc.nextInt();
+            arr[i] = volume;
+            System.out.println("i의 값 : " + i);
+
+            //중복된 값을 찾기 위함.
+            if (i != 0){
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("중복된 값이 있네요! 다시 입력해주세요.");
+                    System.out.print("현재 입력된 값 "+ arr[i]+ " ");
+                    System.out.println();
+                    --i;
+                    System.out.println("나갔어요  " + "i의 값 : " + i);
+                    break;
                 }
             }
-        }
-        //변수를 바꿔야하기에 change 변수를 선언함.
-        int change;
-        //i를 고정값으로 잡고 나머지 인덱스의 수를 비교하는 식을 만듬.
-        for (int i = 0; i < Array.length; i++) {
-            for (int j = 0; j < Array.length; j++) {
-                if (Array[i] < Array[j]) {
-                    /*
-                    변수 교환 공식.
-                    c = a
-                    a = b
-                    b = c
-                     */
-                    change = Array[i];
-                    Array[i] = Array[j];
-                    Array[j] = change;
-                }
             }
         }
-        //출력을 위해 for문을 사용하여 Array 의 모든 값을 출력함.
-        for (int i = 0; i < Array.length; i++)
-        System.out.print(Array[i]);
+        int change = 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i+1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    change = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = change;
+                }
+
+            }
+        }
+        System.out.println("오름차순으로 출력!");
+        //입력된 값 출력.
+     for (int i = 0; i < arr.length; i++)
+         System.out.print(arr[i] + " ");
+    sc.close();
     }
-    //2320110184 김새별
 }
