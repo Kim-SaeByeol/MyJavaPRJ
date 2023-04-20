cba92fefcdcad2a788238a308a094af32d93551a
Kim-SaeByeol <toquf345@gmail.com>
2023-04-20 09:17:18 +0900

    2023.04.20 자바 시험공부

--- src/main/java/Java_Study/Java_Study_2Day.java
+++ src/main/java/Java_Study/Java_Study_2Day.java
@@ -1,54 +1,36 @@
 package Java_Study;
-//for문에 관한.
-
-import java.util.Scanner;
+//if 문과 for문에 대하여
 public class Java_Study_2Day {
-   /*
-5개의 입력 받는 정수를 큰 순서대로 출력하시오.
-코드 한 줄마다 본인이 이해한 내용으로 주석 달아주길 바랍니다.
-
-반드시 배열 사용
-(입력 예) : 100, 200, 100, 300, 400
-(출력 예) : 400, 300, 200, 100, 100
-
-<1> 스캐너 선언
-<2> 배열 선언
-<3> 배열에 값 입력
-<4> 배열 내림차순 정렬
-<5> 출력
-*/
-
-    public void main(String[] args) {
-        //<1> 입력을 받기 위한 스캐너 선언
-        Scanner sc = new Scanner(System.in);
-
-        //<2> 배열 선언. 배열의 길이는 5개로 잡았다.
-        int arr[] = new int[5];
-
-        //<3> 배열에 값 입력
-        for(int i = 0; i < arr.length; i++){
-            int num = sc.nextInt();
-            arr[i] = num;
-        }
-
-        //<4> 배열 내림차순 정렬
-        for(int i = 0; i<arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                int change;
-
-                //내림차순이므로 인덱스가 1 낮은 i가 j보다 작아야됨.
-                if(arr[i] < arr[j]){
-                    change = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = change;
-                }
+    public static void main(String[] args) {
+        if(true) {
+            System.out.println("if절에는 boolear 값인 true 와 false 의 값이 들어가야됨.");
+            int i = 0;
+            for (; ;) {
+                System.out.println(i + "번 반복중");
+                i++;
+                if(i == 10)
+                    break;
+            }
+            System.out.println("1~5 출력하는데 2는 출력하지마");
+            for( i = 1; i<5; i++){
+                if(i == 2)
+                    continue;
+                System.out.println(i);
+            }
+            for (i = 0; i<3; i++){
+                for (int j = 1; j<3; j++)
+                    System.out.println("i 는 현재 :" + i + "이고, j는 현재 : " + j +"입니다.");
+            }
+            System.out.println("증감식을 1의 크기가 아니라 더 크게 잡을 수도 있음. ");
+            for(i = 10; i > 1; i -= 2){
+                System.out.println("현재 i는 :" + i);
             }
         }
-        //<5> 출력
-        for(int i = 0; i < arr.length; i++){
-            System.out.print (arr[i] + " ");
+        int a = 0;
+        if(false) {
+            System.out.println("false 라면 출력되지 않음.");
         }
-        sc.close();
-    }
+        System.out.println("{}의 범위를 항상 잘 보고 {} 안에서 선언한 변수는 해당 {} 안에서만 유효하다는 것을 인지하자.");
 
+    }
 }
