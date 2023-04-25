31ba2a38f0fe191a53df7dcc1b92a3e90bb6a87c
Kim-SaeByeol <toquf345@gmail.com>
2023-04-25 17:15:15 +0900

    23.04.25 자바 for문 복습 소스수정.

--- src/main/java/For_Star/For_Star_test7.java
+++
@@ -1,7 +0,0 @@
-package For_Star;
-
-public class For_Star_test7 {
-
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/Java_Study/Java_Study_6Day.java
+++ src/main/java/Java_Study/Java_Study_6Day.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Java_Study_6Day {
     public static void main(String[] args) {
         int arr[] = new int[10];
-        for (int i = 0; i < arr.length; i++){
-            arr[i]= (int)(Math.random()*100 +1);
+        boolean arr2[] = new boolean[10];
+
+            //값 입력. 1~10 까지 랜덤 값.
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10 + 1);
         }
-        for (int i = 0; i < arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                if(arr[i] > arr[j]){
+
+
+
+            //배열의 순서를 오름차순으로 바꿈
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
                     int change = arr[i];
                     arr[i] = arr[j];
                     arr[j] = change;
@@ -18,11 +25,11 @@ public class Java_Study_6Day {
             }
         }
         System.out.println("[오름차순]");
-        for (int i = 0; i<arr.length; i++)
+        for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
 
 
-        System.out.println();
+            //배열의 순서를 내림차순으로 바꿈
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] < arr[j]) {
@@ -32,8 +39,28 @@ public class Java_Study_6Day {
                 }
             }
         }
+        System.out.println();
         System.out.println("[내림차순]");
-        for (int i = 0; i<arr.length; i++)
+        for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
+
+        
+            //중복인 값을 골라냄
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr2[j] = true;
+                } else {
+                    arr2[j] = false;
+                }
+            }
         }
-}
+            //중복 값을 제외하고 출력 배열 순서는 현재 내림차순이기에 계속 내림차순
+            System.out.println();
+            System.out.println("[중복제거]");
+            for (int i = 0; i < arr.length; i++) {
+                if (arr2[i] == false)
+                    System.out.print(arr[i] + " ");
+            }
+        }
+    }
